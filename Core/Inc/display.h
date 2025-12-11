03a501f7d57442adf89341fc1382c240d6d8ada7
marekquarda <mquarda@centrum.cz>
2025-12-12 00:13:00 +0100

    just traning.

--- Core/Inc/display.h
+++ Core/Inc/display.h
@@ -4,17 +4,149 @@
 #include "main.h"
 #include "stm32l1xx_hal_lcd.h"
 
-
 // macro for argument "symbol" in function display(), their combinations takes symbols for viewing
-#define DOT 		1<<0
-#define COLON 	    1<<1
-#define BAT_0		1<<2
-#define BAT_1		1<<3
-#define BAT_2		1<<4
-#define BAT_3		1<<5
-#define BLINK		1<<6
+// COM0 symbols
+#define COM0_SYMBOL_LIGHT               0x8000000
+#define COM0_SYMBOL_HEART               0x4000000
+#define COM0_NUMBER_ONE                 0x2000000
+#define COM0_NUMBER_1A                  0x1000000
+#define COM0_NUMBER_1E                  0x0008000
+#define COM0_SYMBOL_ARROW_UP            0x0004000
+#define COM0_SYMBOL_LINE_DOWN           0x0002000
+#define COM0_NUMBER_2A                  0x0001000
+#define COM0_NUMBER_2E                  0x0000040
+#define COM0_NUMBER_3A                  0x0000020
+#define COM0_NUMBER_3E                  0x0800000
+#define COM0_SYMBOL_TRIANGLE_UP         0x0400000
+#define COM0_SYMBOL_BASE_DEGREE         0x0000004
+#define COM0_SYMBOL_PERCENTAGE          0x0000002
 
-void display(LCD_HandleTypeDef handle, uint8_t symbol, uint8_t* char_array);
+// COM1 symbols
+#define COM1_SYMBOL_PROBE               0x8000000
+#define COM1_SYMBOL_DASH                0x4000000
+#define COM1_NUMBER_ONE_DOT             0x2000000
+#define COM1_NUMBER_1B                  0x1000000
+#define COM1_NUMBER_1F                  0x0008000
+#define COM1_SYMBOL_TEMP                0x0004000
+#define COM1_SYMBOL_DLINE_DOWN          0x0002000
+#define COM1_NUMBER_2B                  0x0001000
+#define COM1_NUMBER_2F                  0x0000040
+#define COM1_NUMBER_3B                  0x0000020
+#define COM1_NUMBER_3F                  0x0800000
+#define COM1_SYMBOL_TRIANGLE_DOWN       0x0400000
+#define COM1_SYMBOL_DEGREE_FAHR         0x0000004
+#define COM1_SYMBOL_RH                  0x0000002
+
+// COM2 symbols
+#define COM2_SYMBOL_GARBLE_TIME         0x8000000
+#define COM2_SYMBOL_BATTERY             0x4000000
+#define COM2_SYMBOL_DAY_S               0x2000000
+#define COM2_NUMBER_1C                  0x1000000
+#define COM2_NUMBER_1G                  0x0008000
+#define COM2_NUMBER_COLON               0x0004000
+#define COM2_SYMBOL_DLINE_UP            0x0002000
+#define COM2_NUMBER_2C                  0x0001000
+#define COM2_NUMBER_2G                  0x0000040
+#define COM2_NUMBER_3C                  0x0000020
+#define COM2_NUMBER_3G                  0x0800000
+#define COM2_SYMBOL_HEXAGON_FULL        0x0400000
+#define COM2_SYMBOL_DEGREE_CELS         0x0000004
+#define COM2_SYMBOL_HR_S                0x0000002
+
+// COM3 symbols
+#define COM3_SYMBOL_NONE_1              0x8000000
+#define COM3_SYMBOL_TIME                0x4000000
+#define COM3_SYMBOL_NONE_2              0x2000000
+#define COM3_NUMBER_1D                  0x1000000
+#define COM3_NUMBER_1_DOT               0x0008000
+#define COM3_NUMBER_NONE_3              0x0004000
+#define COM3_SYMBOL_AVG                 0x0002000
+#define COM3_NUMBER_2D                  0x0001000
+#define COM3_NUMBER_2_DOT               0x0000040
+#define COM3_NUMBER_3D                  0x0000020
+#define COM3_SYMBOL_MIN                 0x0800000
+#define COM3_SYMBOL_MAX                 0x0400000
+#define COM3_SYMBOL_NONE_4              0x0000004
+#define COM3_SYMBOL_MIN_S               0x0000002
+
+typedef enum {
+    COM0,
+    COM1,
+    COM2,
+    COM3
+} Coms;
+
+// typedef enum {
+
+// } Com0_symbols;
+
+// typedef enum {
+
+// } Com1_symbols;
+
+// typedef enum {
+
+// } Com2_symbols;
+
+// typedef enum {
+
+// } Com3_symbols;
+
+typedef enum {
+    NUMBER_1A = COM0_NUMBER_1A,
+    NUMBER_1B = COM1_NUMBER_1B,
+    NUMBER_1C = COM2_NUMBER_1C,
+    NUMBER_1D = COM3_NUMBER_1D,
+    NUMBER_1E = COM0_NUMBER_1E,
+    NUMBER_1F = COM1_NUMBER_1F,
+    NUMBER_1G = COM2_NUMBER_1G,
+} Position_one;
+
+typedef enum {
+    NUMBER_2A = COM0_NUMBER_2A,
+    NUMBER_2B = COM1_NUMBER_2B,
+    NUMBER_2C = COM2_NUMBER_2C,
+    NUMBER_2D = COM3_NUMBER_2D,
+    NUMBER_2E = COM0_NUMBER_2E,
+    NUMBER_2F = COM1_NUMBER_2F,
+    NUMBER_2G = COM2_NUMBER_2G,
+} Position_two;
+
+typedef enum {
+    NUMBER_3A = COM0_NUMBER_3A,
+    NUMBER_3B = COM1_NUMBER_3B,
+    NUMBER_3C = COM2_NUMBER_3C,
+    NUMBER_3D = COM3_NUMBER_3D,
+    NUMBER_3E = COM0_NUMBER_3E,
+    NUMBER_3F = COM1_NUMBER_3F,
+    NUMBER_3G = COM2_NUMBER_3G,
+} Position_three;
+
+typedef enum {
+    NUMBER_0,
+    NUMBER_1,
+    NUMBER_2,                       
+    NUMBER_3, 
+    NUMBER_4,                        
+    NUMBER_5,                        
+    NUMBER_6,                        
+    NUMBER_7,
+    NUMBER_8,                        
+    NUMBER_9,                        
+} Number;
+
+typedef enum {
+    POSITION_0,
+    POSITION_1,
+    POSITION_2,
+    POSITION_3
+} Position;
+
+
+void display(LCD_HandleTypeDef handle, uint32_t *symbols);
+void clearDisp(uint32_t* symbols);
+void addSymbol(Coms com, uint32_t value, uint32_t *symbols);
+//int* getNumber(Position position, Number number, int* symbols);
 void show(LCD_HandleTypeDef handle);
 
 #endif
--- Core/Src/display.c
+++ Core/Src/display.c
@@ -11,136 +11,125 @@
  *
  * segment map (relation with STM32):
  *  PIN(stm): PA2  PA3   PC4   PC5  PB0  PB1  PB12  PB13  PB14  PB15  PC6   PC7   PC8   PC9
- *        AF: SEG1 SEG2 SEG22 SEG23 SEG5 SEG8 SEG12 SEG13 SEG14 SEG15 SEG24 SEG25 SEG26 SEG27
- * PIN(disp):  5    6     7     8    9   10    11    12    13    14    15   16     17   18
- *      COM0: 5D    --    6D     T4   1D  --    --    2D   COL1   3D    --    4D    DOT1
- *      COM1: 5E    5C    6E     T3   1E  1C    6C    2E    2C    3E    3C    4E    4C
- *      COM2: 5G    5B    6G     T2   1G  1B    6B    2G    2B    3G    3B    4G    4B
- *      COM3: 5F    5A    6F     T1   1F  1A    6A    2F    2A    3F    3A    4F    4A
+ *        AF: SEG1 SEG2 SEG22 SEG23 SEG5 SEG6 SEG12 SEG13 SEG14 SEG15 SEG24 SEG25 SEG26 SEG27
+ * PIN(disp):  14    13    12    11   10   9    8     7     6     5     4     3     2     1
  *
- *      SUMMARY:
- *      digit 1: SEG1 + SEG2
- *      digit 2: SEG22 + SE23
- *      digit 3: SEG5 + SEG8
- *      digit 4: SEG12 + SEG13
- *      digit 5: SEG14  + SE15
- *      sigit 6: SEG24  + SEG25
- *         col1: SEG26
- *         dot1: SEG27
- *      battery: SEG13
  */
-// position COM for segments in order ABCDEFGH (see "segment map")
-const uint8_t com_key[8] = {3,2,1,0,1,3,2,0};
 
-// position cipher (digits) in LCD RAM (see "segment map")
-const uint32_t digit_segment[7][2] = {
-    {1<<1,1<<2},
-	{1<<22,1<<23},
-	{1<<5,1<<8},
-	{1<<12,1<<13},
-	{1<<14, 1<<15},
-	{1<<24, 1<<25},
-	{1<<25, 1<<26}
-};
+uint32_t symbols_mem[3];
 
-// znakovÃ¡ sada
-const uint8_t charset[12]={
- // segmenty: HGFEDCBA
- 	0b00111111, // 0
-	0b00000110, // 1
-	0b01011011, // 2
-	0b01001111,	// 3
-	0b01100110, // 4
-	0b01101101, // 5
-	0b01111101, // 6
-	0b00000111, // 7
-	0b01111111, // 8
-	0b01101111, // 9
-	0b00000000,  // blank
-	0b01000000  // znamenko minus
-};
+void display(LCD_HandleTypeDef handle, uint32_t* symbols) {
+	HAL_LCD_Write(&handle, LCD_RAM_REGISTER0, 0x00000000, symbols[0]);
+	HAL_LCD_Write(&handle, LCD_RAM_REGISTER2, 0x00000000, symbols[1]);
+	HAL_LCD_Write(&handle, LCD_RAM_REGISTER4, 0x00000000, symbols[2]);
+	HAL_LCD_Write(&handle, LCD_RAM_REGISTER6, 0x00000000, symbols[3]);
 
-uint8_t disp[6]={0,0,0,0,0,0}; // "defaultnÃ­" hodnota na displeji ("0.00")
-uint8_t i;
-
-void display(LCD_HandleTypeDef handle, uint8_t symbol, uint8_t* char_array) {
-    uint32_t com[4]={0,0,0,0}; // budoucÃ­ obsah RAM LCD driveru
-	uint8_t charmask,i=0,j;
-
-	// tento blok se prostÄ› nedÃ¡ okomentovat :D jde o to pÅ™epsat "segment map" do RAM
-	for(j=0;j<7;j++){ // pro kaÅ¾dou ÄÃ­slici
-		charmask=charset[char_array[j]];
-		for(i=0;i<3;i++){
-			if(charmask & 1<<i){
-				com[com_key[i]] |= digit_segment[j][1];
-			}
-		}
-
-		for(i=3;i<8;i++){
-			if(charmask & 1<<i){
-				com[com_key[i]] |= digit_segment[j][0];
-			}
-		}
-	}
-
-	if(symbol & BAT_0){com[3] |= 1<<13;} // symbol "obalu baterie" T1 (na COM3 SEG13)
-	if(symbol & BAT_1){com[0] |= 1<<13;} // "jedna ÄÃ¡rka" T4
-	if(symbol & BAT_2){com[1] |= 1<<13;} // "druhÃ¡ ÄÃ¡rka" T3
-	if(symbol & BAT_3){com[2] |= 1<<13;} // "tÅ™etÃ­ ÄÃ¡rka"	T2
-	if(symbol & DOT){com[0] |= 1<<31;} // desetinnÃ¡ teÄka
-	if(symbol & COLON){com[0] |= 1<<0;} // dvojteÄka
-
-	// jestli se jeÅ¡tÄ› nestihl dokonÄit poslednÃ­ zÃ¡pis do LCD RAM tak poÄkej
-	//while(HAL_LCD_GetState(LCD_FLAG_UDR) != HAL_LCD_ERROR_UDR){};
-	//write to RAM
-	// HAL_LCD_Write(&handle, LCD_RAM_REGISTER0, 0xF00F0066, com[0]);
-	// HAL_LCD_Write(&handle, LCD_RAM_REGISTER2, 0xF00F0066, com[1]);
-	// HAL_LCD_Write(&handle, LCD_RAM_REGISTER4, 0xF00F0066, com[2]);
-	// HAL_LCD_Write(&handle, LCD_RAM_REGISTER6, 0xF00F0066, com[3]);
-
-	HAL_LCD_Write(&handle, LCD_RAM_REGISTER0, 0x00000000, 0x0FC0F066);
-	HAL_LCD_Write(&handle, LCD_RAM_REGISTER2, 0x00000000, 0x0FC0F066);
-	HAL_LCD_Write(&handle, LCD_RAM_REGISTER4, 0x00000000, 0x0FC0F066);
-	HAL_LCD_Write(&handle, LCD_RAM_REGISTER6, 0x00000000, 0x05C0B062);
+	// HAL_LCD_Write(&handle, LCD_RAM_REGISTER0, 0x00000000, 0x0FC0F066);
+	// HAL_LCD_Write(&handle, LCD_RAM_REGISTER2, 0x00000000, 0x0FC0F066);
+	// HAL_LCD_Write(&handle, LCD_RAM_REGISTER4, 0x00000000, 0x0FC0F066);
+	// HAL_LCD_Write(&handle, LCD_RAM_REGISTER6, 0x00000000, 0x05C0B062);
 
 	HAL_LCD_UpdateDisplayRequest(&handle); 
-
-	// HAL_Delay(1000);
-	// HAL_LCD_Write(&handle, LCD_RAM_REGISTER0, 0x00000006, 0x0000FFFF);
-	// HAL_LCD_Write(&handle, LCD_RAM_REGISTER2, 0x0F00F066, 0x0000FFFF);
-	// HAL_LCD_Write(&handle, LCD_RAM_REGISTER4, 0x0F00F066, 0x0000FFFF);
-	// HAL_LCD_Write(&handle, LCD_RAM_REGISTER6, 0x0F00F066, 0x0000FFFF);
-
-	// // Update the LCD display 
-	// HAL_LCD_UpdateDisplayRequest(&handle); 
-
-	// HAL_Delay(1000);
-	// HAL_LCD_Write(&handle, LCD_RAM_REGISTER0, 0x0F00F066, 0xFFFF0000);
-	// HAL_LCD_Write(&handle, LCD_RAM_REGISTER2, 0x0F00F066, 0xFFFF0000);
-	// HAL_LCD_Write(&handle, LCD_RAM_REGISTER4, 0x0F00F066, 0xFFFF0000);
-	// HAL_LCD_Write(&handle, LCD_RAM_REGISTER6, 0x0F00F066, 0xFFFF0000);
-	// HAL_LCD_UpdateDisplayRequest(&handle); 
 }
 
 void show(LCD_HandleTypeDef handle) {
+	clearDisp(symbols_mem);
 	//display(handle,0,disp); // "shows" default content of display
-
+    addSymbol(COM0, COM0_SYMBOL_HEART, symbols_mem);
+	display(handle, symbols_mem);
 	while(1){
 	//	HAL_Delay(1000);
 		// if(TIM_GetFlagStatus(TIM7,TIM_FLAG_Update) != RESET){ // pokud pÅ™etekl timer
 		// 	TIM_ClearFlag(TIM7,TIM_FLAG_Update); // vyÄistÃ­me vlajku
-			// inkrementujeme poÄÃ­tadlo na displeji
-			disp[5]++; // trocha matematiky ve stylu "jedna jde dÃ¡l"
-			if(disp[5]>9){disp[5]=0;disp[4]++;}
-			if(disp[4]>9){disp[4]=0;disp[3]++;}
-			if(disp[3]>9){disp[3]=0;disp[2]++;}
-			if(disp[2]>9){disp[2]=0;disp[1]++;}
-			if(disp[1]>9){disp[1]=0;disp[0]++;}
-			if(disp[0]>9){
-				for(i=0;i<6;i++){disp[i]=0;} // napoÄÃ­tali jsme 999999s (to se vÃ¡m asi nestane)
-			}
-			display(handle,0,disp); // "zobrazÃ­me" hodnoty na displeji (pÅ™epÃ­Å¡eme obsah LCD RAM)
+			//display(handle,0,disp); // "zobrazÃ­me" hodnoty na displeji (pÅ™epÃ­Å¡eme obsah LCD RAM)
 		// }
+
 	}
 }
+
+void addSymbol(Coms com, uint32_t value, uint32_t *symbols) {
+	switch (com)
+	{
+	case COM0:
+		symbols[0] = value;
+		break;
+	case COM1:
+		symbols[1] = value;
+		break;
+	case COM2:
+		symbols[2] = value;
+		break;		
+	case COM3:
+		symbols[3] = value;
+		break;		
+	default:
+		break;
+	}
+}
+
+void clearDisp(uint32_t* symbols) {
+	for(int i = 0; i <= 3; ++i) {
+		symbols[i] = 0;
+	}
+}
+
+// int* showNumber(Position position, Number number, uint32_t* symbols[3]) {
+// 	int* temp[3];
+
+// 	switch (position)
+// 	{
+// 	case POSITION_0:
+// 		switch (number) 
+// 		{
+// 		case NUMBER_0:
+			
+// 			break;
+// 		case NUMBER_1:
+			
+// 			break;
+// 		case NUMBER_2:
+			
+// 			break;							
+
+// 		case NUMBER_3:
+			
+// 			break;
+// 		case NUMBER_4:
+			
+// 			break;
+// 		case NUMBER_5:
+			
+// 			break;
+// 		case NUMBER_6:
+			
+// 			break;							
+// 		case NUMBER_7:
+			
+// 			break;
+// 		case NUMBER_8:
+			
+// 			break;
+// 		case NUMBER_9:
+			
+// 			break;
+// 		default:
+// 			break;
+// 		}
+// 		break;
+// 	case POSITION_1:
+
+// 		break;
+
+// 	case POSITION_2:
+	
+// 		break;
+
+// 	case POSITION_3:
+	
+// 		break;
+// 	default:
+// 		break;
+// 	}
+// 	return &temp;
+// }
 
