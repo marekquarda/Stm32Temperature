58554c0c6fa7a1c2769941e09b63355a12171cc7
marekquarda <mquarda@centrum.cz>
2025-12-15 22:37:43 +0100

    base for virtual usb com port

--- USB_DEVICE/App/usbd_cdc_if.h
+++ USB_DEVICE/App/usbd_cdc_if.h
@@ -1,9 +1,9 @@
 /* USER CODE BEGIN Header */
 /**
   ******************************************************************************
-  * @file           : usbd_storage_if.h
+  * @file           : usbd_cdc_if.h
   * @version        : v2.0_Cube
-  * @brief          : Header for usbd_storage_if.c file.
+  * @brief          : Header for usbd_cdc_if.c file.
   ******************************************************************************
   * @attention
   *
@@ -20,15 +20,15 @@
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 
-#ifndef __USBD_STORAGE_IF_H__
-#define __USBD_STORAGE_IF_H__
+#ifndef __USBD_CDC_IF_H__
+#define __USBD_CDC_IF_H__
 
 #ifdef __cplusplus
  extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
-#include "usbd_msc.h"
+#include "usbd_cdc.h"
 
 /* USER CODE BEGIN INCLUDE */
 
@@ -39,16 +39,18 @@
   * @{
   */
 
-/** @defgroup USBD_STORAGE USBD_STORAGE
-  * @brief Header file for the usb_storage_if.c file
+/** @defgroup USBD_CDC_IF USBD_CDC_IF
+  * @brief Usb VCP device module
   * @{
   */
 
-/** @defgroup USBD_STORAGE_Exported_Defines USBD_STORAGE_Exported_Defines
+/** @defgroup USBD_CDC_IF_Exported_Defines USBD_CDC_IF_Exported_Defines
   * @brief Defines.
   * @{
   */
-
+/* Define size for the receive and transmit buffer over CDC */
+#define APP_RX_DATA_SIZE  1024
+#define APP_TX_DATA_SIZE  1024
 /* USER CODE BEGIN EXPORTED_DEFINES */
 
 /* USER CODE END EXPORTED_DEFINES */
@@ -57,7 +59,7 @@
   * @}
   */
 
-/** @defgroup USBD_STORAGE_Exported_Types USBD_STORAGE_Exported_Types
+/** @defgroup USBD_CDC_IF_Exported_Types USBD_CDC_IF_Exported_Types
   * @brief Types.
   * @{
   */
@@ -70,7 +72,7 @@
   * @}
   */
 
-/** @defgroup USBD_STORAGE_Exported_Macros USBD_STORAGE_Exported_Macros
+/** @defgroup USBD_CDC_IF_Exported_Macros USBD_CDC_IF_Exported_Macros
   * @brief Aliases.
   * @{
   */
@@ -83,13 +85,13 @@
   * @}
   */
 
-/** @defgroup USBD_STORAGE_Exported_Variables USBD_STORAGE_Exported_Variables
+/** @defgroup USBD_CDC_IF_Exported_Variables USBD_CDC_IF_Exported_Variables
   * @brief Public variables.
   * @{
   */
 
-/** STORAGE Interface callback. */
-extern USBD_StorageTypeDef USBD_Storage_Interface_fops_FS;
+/** CDC Interface callback. */
+extern USBD_CDC_ItfTypeDef USBD_Interface_fops_FS;
 
 /* USER CODE BEGIN EXPORTED_VARIABLES */
 
@@ -99,10 +101,12 @@ extern USBD_StorageTypeDef USBD_Storage_Interface_fops_FS;
   * @}
   */
 
-/** @defgroup USBD_STORAGE_Exported_FunctionsPrototype USBD_STORAGE_Exported_FunctionsPrototype
+/** @defgroup USBD_CDC_IF_Exported_FunctionsPrototype USBD_CDC_IF_Exported_FunctionsPrototype
   * @brief Public functions declaration.
   * @{
   */
+
+uint8_t CDC_Transmit_FS(uint8_t* Buf, uint16_t Len);
 
 /* USER CODE BEGIN EXPORTED_FUNCTIONS */
 
@@ -124,5 +128,5 @@ extern USBD_StorageTypeDef USBD_Storage_Interface_fops_FS;
 }
 #endif
 
-#endif /* __USBD_STORAGE_IF_H__ */
+#endif /* __USBD_CDC_IF_H__ */
 
