65d131e33e3d49b8675e7ff80caf80c3deca8ae0
marekquarda <mquarda@centrum.cz>
2025-12-18 00:11:02 +0100

    Read, Read Fast SPI EEPROM implementation

--- Core/Inc/spi_eeprom.h
+++ Core/Inc/spi_eeprom.h
@@ -13,12 +13,19 @@
 extern SPI_HandleTypeDef hspi1;
 #define W25X_SPI hspi1
 
+#define numBLOCK            8       // Number of total blocks for 4Mb flash 8x16x16x256
+#define JEDEC_ID            0x9F    // JEDEC ID
+#define READ_DATA           0x03    // Read data
+#define FAST_READ           0x0B    // Fast Read data
+
 #define csLOW() HAL_GPIO_WritePin(GPIOA, SPI1_CS_Pin, GPIO_PIN_RESET);
 #define csHIGH() HAL_GPIO_WritePin(GPIOA, SPI1_CS_Pin, GPIO_PIN_SET);
 
-#define numBLOCK    8       // Number of total blocks for 4Mb flash
-#define JEDEC_ID    0x9F    // JEDEC ID
-
 uint32_t W25X_ReadID(void);
+void W25X_Read(uint32_t startPage, uint8_t offset, uint16_t size, uint8_t *rData);
+void W25X_ReadFast(uint32_t startPage, uint8_t offset, uint16_t size, uint8_t *rData);
+void SPI_Write(uint8_t *data, uint8_t length);
+void SPI_Read(uint8_t *data, uint32_t length);
+
 
 #endif
--- Core/Src/spi_eeprom.c
+++ Core/Src/spi_eeprom.c
@@ -12,7 +12,7 @@ void SPI_Write(uint8_t *data, uint8_t length) {
     HAL_SPI_Transmit(&W25X_SPI, data, length, 2000);
 };
 
-void SPI_Read(uint8_t *data, uint8_t length) {
+void SPI_Read(uint8_t *data, uint32_t length) {
     HAL_SPI_Receive(&W25X_SPI, data, length, 5000);
 };
 
@@ -25,4 +25,59 @@ uint32_t W25X_ReadID(void)
     HAL_SPI_Receive(&W25X_SPI, rData, 3, 3000);
     csHIGH();
     return ((rData[0]<< 16) | (rData[1]<< 8) | rData[2]);
+}
+
+void W25X_Read(uint32_t startPage, uint8_t offset, uint16_t size, uint8_t *rData) {
+    uint8_t tData[5];
+    uint32_t memAddr = startPage*256 + offset;
+    if (numBLOCK < 512) {
+        tData[0] = READ_DATA;               // Enable Read data
+        tData[1] = (memAddr>>16)&0xFF;      // MSB of the memory Address
+        tData[2] = (memAddr>>8)&0xFF;
+        tData[3] = (memAddr)&0xFF;          // LSB of the memory Address    
+    } else {
+        tData[0] = READ_DATA;               // Enable Read data
+        tData[1] = (memAddr>>24)&0xFF;      // MSB of the memory Address
+        tData[2] = (memAddr>>16)&0xFF;
+        tData[3] = (memAddr>>8)&0xFF;       
+        tData[4] = (memAddr)&0xFF;          // LSB of the memory Address    
+    }
+    
+    csLOW();
+    if(numBLOCK < 512) {
+        SPI_Write(tData, 4);
+    } else {
+        SPI_Write(tData, 5);
+    }
+    SPI_Read(rData, size);
+    csHIGH();
+}
+
+
+void W25X_FastRead(uint32_t startPage, uint8_t offset, uint16_t size, uint8_t *rData) {
+    uint8_t tData[6];
+    uint32_t memAddr = startPage*256 + offset;
+    if (numBLOCK < 512) {
+        tData[0] = FAST_READ;               // Enable Fast Read
+        tData[1] = (memAddr>>16)&0xFF;      // MSB of the memory Address
+        tData[2] = (memAddr>>8)&0xFF;
+        tData[3] = (memAddr)&0xFF;          // LSB of the memory Address    
+        tData[4] = 0;                       // Dummy clock
+    } else {
+        tData[0] = FAST_READ;               // Enable Fast Read
+        tData[1] = (memAddr>>24)&0xFF;      // MSB of the memory Address
+        tData[2] = (memAddr>>16)&0xFF;
+        tData[3] = (memAddr>>8)&0xFF;       
+        tData[4] = (memAddr)&0xFF;          // LSB of the memory Address   
+        tData[5] = 0;                       // Dummy clock
+    }
+    
+    csLOW();
+    if(numBLOCK < 512) {
+        SPI_Write(tData, 5);
+    } else {
+        SPI_Write(tData, 6);
+    }
+    SPI_Read(rData, size);
+    csHIGH();
 }
